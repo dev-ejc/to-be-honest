13132a59fa70d9d1ea2041b82cb9897e30b025b2
ericjchung <ericjchung@protonmail.com>
2019-08-09 05:23:33 +0000

    Changed Title

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">WIN</a>
+  <a class="navbar-brand" href="/">What is New</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
