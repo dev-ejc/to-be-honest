031a96c5d0003aa4cafb37ea929a1d5735b5829d
ericjchung <ericjchung@protonmail.com>
2019-08-12 07:02:54 +0000

    Removed api file

--- whatisnew/news/views.py
+++ whatisnew/news/views.py
@@ -9,14 +9,14 @@ from textblob import TextBlob
 import datetime
 import datetime as dt
 import newsapi as na
-
+import os
 from newsapi import NewsApiClient
 
 def newsView(request,topic):
-    newsapi = NewsApiClient(api_key='504dda888b214fb89ee41e2a67a4b8bf')
-    top_headlines = newsapi.get_top_headlines(q="Election",
+    newsapi = NewsApiClient(api_key=os.getenv("NEWS_KEY",'Token Not Found'))
+    top_headlines = newsapi.get_top_headlines(q=topic,
                                           category='business',
                                           language='en',
                                           country='us')
-    print(top_headlines)
+    print(top_headlines.articles[0])
     return HttpResponse("Nice")
