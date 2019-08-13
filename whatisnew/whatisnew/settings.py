1ad45185d2f97bbac93ecc699593189fed93403f
ericjchung <ericjchung@protonmail.com>
2019-08-13 07:09:45 +0000

    Deployment attempt iv

--- whatisnew/whatisnew/settings.py
+++ whatisnew/whatisnew/settings.py
@@ -119,5 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
