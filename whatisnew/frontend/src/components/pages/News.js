14500c0988eb04db3d6c10308d287479c49b5012
ericjchung <ericjchung@protonmail.com>
2019-08-17 18:20:53 +0000

    Centered load

--- whatisnew/frontend/src/components/pages/News.js
+++ whatisnew/frontend/src/components/pages/News.js
@@ -17,8 +17,10 @@ const News = () => {
     <div className="card">
       <div className="card-body">
       {loading || ts === null || topic === '' ? (
-        <div class="spinner-border mx-auto text-primary" role="status">
+        <div className="d-flex justify-content-center">
+        <div class="spinner-border text-primary" role="status">
           <span class="sr-only">Loading...</span>
+        </div>
         </div>
       ) : (
         <div>
